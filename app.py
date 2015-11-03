ceae1d26af221acf6efff97c8e7a3e9dd17e4d97
Stuart Munro <stuart@munro.co>
2015-11-03 18:09:22 +0000

    add error to highlight

--- app.py
+++ app.py
@@ -5,5 +5,12 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+
+@app.route('/error')
+def error():
+    c = y
+    return 'Should error!'
+
 if __name__ == '__main__':
+    app.debug = True
     app.run(host='0.0.0.0')
